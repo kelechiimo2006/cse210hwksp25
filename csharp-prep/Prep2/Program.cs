e9ba500bfabd5a194c1bc11497e2460b4c0b8213
Samuel-David Kelechi Imo <imo23002@byui.edu>
2025-04-26 21:52:54 -0600

    Finished Prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        Console.Write("What is your letter grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your letter grade is {letter}");
+    
+        Console.WriteLine("");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats you passed the class");
+        }
+        else
+        {
+            Console.WriteLine("You failed but do better next time");
+        }    
     }
 }
